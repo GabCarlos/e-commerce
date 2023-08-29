42bea6f38e784555b7dea6154ae1311139201af8
Your Name <you@example.com>
2023-08-28 22:26:21 -0300

    commit

--- src/context/appcontext.js
+++ src/context/appcontext.js
@@ -1,6 +1,28 @@
-//Criando o Context para o código:
-import { createContext } from 'react';
+import React, { createContext, useContext, useState } from "react";
 
 const AppContext = createContext();
+
+export function AppProvider({ children }) {
+  const [cartItems, setCartItems] = useState([]); // Inicialize o estado com um array vazio
+
+  const addToCart = (item) => {
+    setCartItems([...cartItems, item]);
+  };
+
+  const contextValues = {
+    cartItems,
+    addToCart,
+  };
+
+  return (
+    <AppContext.Provider value={contextValues}>
+      {children}
+    </AppContext.Provider>
+  );
+}
+
+export function useAppContext() {
+  return useContext(AppContext);
+}
 
 export default AppContext;
