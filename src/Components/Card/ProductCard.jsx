830a60deadfbe47464ddf50798773fe3cffa9aca
Your Name <you@example.com>
2023-09-18 21:49:21 -0300

    commit

--- src/Components/Card/ProductCard.jsx
+++ src/Components/Card/ProductCard.jsx
@@ -1,46 +1,38 @@
 import React from "react";
-import './ProductCard.css';
-import {BsFillCartFill} from 'react-icons/bs'
-import propTypes from 'prop-types'
+import "./ProductCard.css";
+import { BsFillCartFill } from "react-icons/bs";
+import propTypes from "prop-types";
 import formatCurrency from "../../utils/formatCurrency";
 import { useContext } from "react";
 import AppContext from "../../context/appcontext";
 
-//Função que está passando os produtos junto com os seus valores, descrições e imagens:
 function ProductCard({ data }) {
+  const { title, thumbnail, price } = data;
 
-  const { title, thumbnail, price } = data; //Const base das informações.
+  const { cartItems, setcartItems } = useContext(AppContext);
 
-  const { cartItems, setcartItems} = useContext(AppContext);
+  const handleAddCart = () => {
+    setcartItems([...cartItems, data]);
+  };
 
-  const handleAddCart = () => { setcartItems([ ...cartItems, data ]) };
-
-//Passando o valor, titulo e nome do item:  
-  return(
-
+  return (
     <section className="ProductCard">
-     
-      <img src={thumbnail.replace(/\w\.jpg/gi, 'W.jpg')} alt="product" className="img"/>
-
-      <div className="infos">
-        <h2 className="price"> {formatCurrency(price, 'BRL')}</h2>
-        <h2 className="title"> {title} </h2>
+      <div className="product-details">
+        <img src={thumbnail.replace(/\w\.jpg/gi, "W.jpg")} alt="product" className="img" />
+        <div className="infos">
+          <h2 className="price">{formatCurrency(price, "BRL")}</h2>
+          <h2 className="title">{title}</h2>
+        </div>
       </div>
-
-      <button
-       type="button"
-       className="AddCard"
-       onClick={handleAddCart}
-      ><BsFillCartFill/></button>
-
+      <button type="button" className="AddCard" onClick={handleAddCart}>
+        <BsFillCartFill />
+      </button>
     </section>
   );
-};
+}
 
-export default ProductCard;
-
-//Passando o componente props e dados:
 ProductCard.propTypes = {
   data: propTypes.shape({}),
-
 }.isRequired;
+
+export default ProductCard;
